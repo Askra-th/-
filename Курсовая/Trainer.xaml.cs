34c082b4fa2dce83d61ab1559e27a965225cf5e2
svbch <svbch@DESKTOP-F61KJS2>
2024-12-17 20:51:10 +0300

    Добавьте файлы проекта.

---
+++ Курсовая/Trainer.xaml.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Курсовая
+{
+    /// <summary>
+    /// Логика взаимодействия для Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        DispatcherTimer dt = new DispatcherTimer(); // класс для создание таймера
+        Stopwatch sw = new Stopwatch(); //класс для получение прошедшего времени
+        string currentTime = string.Empty;
+        public Window1(string title)
+        {
+            this.Symbols();
+            InitializeComponent();
+            dt.Tick += new EventHandler(dt_Tick);
+            dt.Interval = new TimeSpan(0, 0, 0, 0, 1);
+            OUT.Text = Code;
+            Miss.Text = "0";
+            Title = title;
+            this.DataContext = this;
+            sw.Start();
+            dt.Start();
+
+        }
+        void dt_Tick(object sender, EventArgs e)
+        {
+            if (sw.IsRunning) //свойство, если таймер идёт
+            {
+                TimeSpan ts = sw.Elapsed;
+                currentTime = String.Format("{0:00}:{1:00}:{2:00}",
+                ts.Minutes, ts.Seconds, ts.Milliseconds / 10);
+                if (ts.Minutes >= 1000|| Mistakes > 100)
+                {
+                    
+                    End(ts.Minutes);
+                }
+                timer.Text = currentTime;
+               
+            }
+        }
+
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            if (INPUT.Text.Length == 4 ) 
+            {
+                Check(); 
+                this.Symbols();
+                OUT.Text = Code;
+                INPUT.Clear();
+                End();  
+            }
+
+        }
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            Итоги window = new Итоги(Mistakes, timer.Text);
+
+            Restart();
+        }
+        private string Code { get; set; }
+        private static int Mistakes { get; set; } = 0;
+        private static int Limit { get; set; } = 100;
+        private void Symbols() 
+        {
+             Code= "";   
+                Random rand = new Random();
+                int n = 4;
+                char[] a = new char[4];
+                
+                    for (int i = 0; i < n; i++)
+                    {
+                        a[i] = (char)rand.Next(0x0410, 0x44F);
+                        Code+=a[i] + " ";
+                    } 
+            
+        }
+        private void Origin()
+        {
+            sw.Stop();
+            
+            Mistakes = 0;
+            Limit = 100;
+            Miss.Text = "0";
+            timer.Text= "00:00:00";
+            OUT.Text="";
+        }
+        private void Restart()
+        {
+            this.Symbols();
+            OUT.Text = Code;
+            sw.Restart();
+        }
+        private void End() 
+        {
+            Limit -= 4;
+            if (Limit == 0)
+            {
+                Итоги window = new Итоги(Mistakes,timer.Text);
+                Origin();
+                window.ShowDialog();
+                Restart();
+                
+            }
+         
+
+        }
+        private void End(int time)
+        {
+            
+                Итоги window = new Итоги(Mistakes+101,timer.Text,time);
+                Origin();
+                window.ShowDialog();
+                Restart();
+
+            
+
+
+        }
+        private void Check()
+        {
+            string inspector = Code.Replace(" ", "");
+            string checking = INPUT.Text;
+
+            for (int i = 0; i < 4; i++)
+            {
+                if (inspector[i] != checking[i])
+                {
+                    Mistakes++;
+                }
+            }
+            Miss.Text=Convert.ToString(Mistakes);
+
+        }
+        
+       
+    }
+}
+
+    
+
---
+++ Курсовая/Итоги.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Курсовая
+{
+    /// <summary>
+    /// Логика взаимодействия для Итоги.xaml
+    /// </summary>
+    public partial class Итоги : Window
+    {
+        public Итоги(int miss, string time)
+        {
+            InitializeComponent();
+            res1.Text = Convert.ToString(miss);
+            res2.Text = Convert.ToString(time);
+        }
+        public Итоги(int miss, string time,int time1)
+        {
+            InitializeComponent();
+            if (miss !> 100 || time1 !>= 1000)
+            {
+                res1.Text = Convert.ToString(miss);
+                res2.Text = Convert.ToString(time);
+            }
+            else
+            {
+                res1.Text = ("Stop");
+                res2.Text = ("Stop");
+            }
+        }
+
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            WindowCollection windows = Application.Current.Windows; //доступ ко всем окнам
+            foreach (Window window in windows)
+            {
+                window.Close();
+            }
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            
+            this.Close();
+        }
+    }
+}
